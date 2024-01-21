a181c5839438b6820ea52c6e60a4a11bb06f8eb9
Prince0027 <pk05318@gmail.com>
2024-01-21 20:18:32 +0530

    end of section 7

--- API/DTOs/RegisterDto.cs
+++ API/DTOs/RegisterDto.cs
@@ -8,6 +8,7 @@ namespace API.DTOs
         public string Username {get; set; }
 
         [Required]
+        [StringLength(8, MinimumLength = 4)]  //max is 8 and min is 4
         public string Password {get; set; }
     }
 }
